392a0899c3129af4778293496f40cbad84581e5d
Fidel Eliseo Reyes Ramirez <fideleliseoreyesramirez@gmail.com>
2025-08-08 18:54:22 -0400

    Lenguaje español

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+
 <head>
     @include('partials.head')
 </head>
+
 <body class="min-h-screen bg-white dark:bg-zinc-800">
     <!-- SIDEBAR -->
     <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
         <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
         <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
-            <x-app-logo />
+            <span class="font-bold text-lg text-gray-800 dark:text-gray-200">
+               Acceso de Ascensores
+            </span>
         </a>
+
 
         <flux:navlist variant="outline">
             <flux:navlist.group :heading="__('Gestión')" class="grid">
-                <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
-                    {{ __('Dashboard') }}
-                </flux:navlist.item>
 
-                <flux:navlist.item icon="user" :href="route('usuarios.index')" :current="request()->routeIs('usuarios.*')" wire:navigate>
+                <flux:navlist.item icon="user" :href="route('usuarios.index')"
+                    :current="request()->routeIs('usuarios.*')" wire:navigate>
                     {{ __('Usuarios') }}
                 </flux:navlist.item>
 
-                <flux:navlist.item icon="cog" :href="route('ascensores.index')" :current="request()->routeIs('ascensores.*')" wire:navigate>
+                <flux:navlist.item icon="cog" :href="route('ascensores.index')"
+                    :current="request()->routeIs('ascensores.*')" wire:navigate>
                     {{ __('Ascensores') }}
                 </flux:navlist.item>
             </flux:navlist.group>
@@ -32,18 +36,16 @@
 
         <!-- MENU DE USUARIO (ESCRITORIO) -->
         <flux:dropdown class="hidden lg:block" position="bottom" align="start">
-            <flux:profile
-                :name="auth()->user()->name"
-                :initials="auth()->user()->initials()"
-                icon:trailing="chevrons-up-down"
-            />
+            <flux:profile :name="auth()->user()->name" :initials="auth()->user()->initials()"
+                icon:trailing="chevrons-up-down" />
 
             <flux:menu class="w-[220px]">
                 <flux:menu.radio.group>
                     <div class="p-0 text-sm font-normal">
                         <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
                             <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                <span class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                <span
+                                    class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                                     {{ auth()->user()->initials() }}
                                 </span>
                             </span>
@@ -81,17 +83,15 @@
         <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
         <flux:spacer />
         <flux:dropdown position="top" align="end">
-            <flux:profile
-                :initials="auth()->user()->initials()"
-                icon-trailing="chevron-down"
-            />
+            <flux:profile :initials="auth()->user()->initials()" icon-trailing="chevron-down" />
 
             <flux:menu>
                 <flux:menu.radio.group>
                     <div class="p-0 text-sm font-normal">
                         <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
                             <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                <span class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                <span
+                                    class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                                     {{ auth()->user()->initials() }}
                                 </span>
                             </span>
@@ -129,4 +129,5 @@
 
     @fluxScripts
 </body>
+
 </html>
